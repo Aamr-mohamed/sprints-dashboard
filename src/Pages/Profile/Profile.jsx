8a5b399ad0ddc1e5acad96ca81d7e52291e5abc1
RanaTahon <111710924+RanaTahon@users.noreply.github.com>
2024-04-07 05:04:13 +0100

    Create basic profile page

--- src/Pages/Profile/Profile.jsx
+++ src/Pages/Profile/Profile.jsx
@@ -3,13 +3,29 @@ import { useParams } from "react-router-dom";
 import Layout from "../../Layout/Layout";
 
 function Profile() {
+	const usersList = JSON.parse(localStorage.getItem("users") || "[]")
 	const { userId } = useParams()
-	return (
-		<Layout>
-			<h1 className="text-blue-600">Profile</h1>
-			<h1 className="text-blue-600">User id : {userId}</h1>
-		</Layout>
-	)
+	const user = usersList[userId]
+	if (user) {
+		return (
+			<Layout>
+				<h1 className="text-blue-600">Profile</h1>
+				<h1 className="text-blue-600">User id : {userId}</h1>
+				<h1>Address: {user.address}</h1>
+				<h1>Country: {user.country}</h1>
+				<h1>Email: {user.email}</h1>
+				<h1>Fullname: {user.fullname}</h1>
+				<h1>Phone Number: {user.phoneNumber}</h1>
+			</Layout>
+		)
+	}
+	else {
+		return (
+			<Layout>
+				<h1>User Not Found</h1>
+			</Layout>
+		)
+	}
 }
 
 export default Profile
